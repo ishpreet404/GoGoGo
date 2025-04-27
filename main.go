8333daaa8e69b988c137e35d139b43ce904fa5e5
Ishpreet Singh <ishpreet@outlook.in>
2025-04-27 11:26:06 +0530

    variables and zero values

--- main.go
+++ main.go
@@ -5,5 +5,22 @@ import (
 )
 
 func main() {
-	fmt.Println("GOGOGO")
+	var name string = "Ishpreet"
+	fmt.Printf("This is my name %s\n ",name)
+	age:= 19   // shorthand operator
+	fmt.Printf("This is my age %d\n",age)
+	var city string
+	city = "Delhi"
+	fmt.Printf("This is my city %s\n",city)
+
+	var (
+		isEmployed bool =true
+		salary int = 11000
+		position string = "developer"
+	)
+	fmt.Printf("isemployes %t at position : %s with salary : %d\n",isEmployed,position,salary)
+	// Zero values
+	var defaultint int
+
+	fmt.Printf("int %d",defaultint)
 }
